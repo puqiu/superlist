d31f07eaec3e55b2dba173634bccfaded8c98cf5
PuQiu <puqiuster@gmail.com>
2016-12-02 23:12:12 +0800

    change functional test to Django CaseTest

---
+++ functional_test/test.py
@@ -0,0 +1,29 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn('TO-DO', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows =  self.browser.find_elements_by_tag_name('tr')
+        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
